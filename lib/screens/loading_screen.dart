f92e481842d9b64cffd989f16814cf22f93277be
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-22 01:02:51 +0600

    Getting current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +7,21 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,17 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double longitude;
+  double latitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      this.longitude = position.longitude;
+      this.latitude = position.latitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
